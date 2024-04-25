5a9d88b25a6a7694ad2a5e0c5306817cb698c065
thedoynewton <thedoynewton@gmail.com>
2024-04-26 00:25:39 +0800

    add projects section

--- src/main.js
+++ src/main.js
@@ -4,4 +4,5 @@ import '../styles/components/header.css';
 import '../styles/components/hero.css';
 import '../styles/components/about.css';
 import '../styles/components/featured.css';
+import '../styles/components/project.css';
 import '../styles/utils.css';
