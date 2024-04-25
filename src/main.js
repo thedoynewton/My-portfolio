97ea2f6c9350a1a80f93a03ce45b4d57672de750
thedoynewton <thedoynewton@gmail.com>
2024-04-25 20:43:00 +0800

    add featured section

--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ import '../styles/style.css';
 import '../styles/components/header.css';
 import '../styles/components/hero.css';
 import '../styles/components/about.css';
-import '../styles/utils.css'
+import '../styles/components/featured.css';
+import '../styles/utils.css';
